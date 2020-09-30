297515055d97585065537448188f3bb2d709ab98
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2020-09-30 16:13:44 +0300

    Data

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,7 @@
-import posts from './data.js'
+import post from './data.js'
+
+console.log(post.author.name);
+console.log(post.author.lastname);
+console.log(post.content.background);
+console.log(post.content.images[0]);
+
